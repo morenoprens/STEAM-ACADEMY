c77ea8592afec7ab01d7fa9b27f78b7e25cee6ef
camilo Moreno <morenoprens@yahoo.es>
2024-07-26 19:37:52 -0500

    repositorio primer proyecto en STEAM ACADEMY

---
+++ script.js
@@ -0,0 +1,10 @@
+function sumar(){
+
+ let num1 = parseFloat(document.getElementById('numero1').value) ;
+   
+ let num2 = parseFloat( document.getElementById ('numero2').value); 
+
+ let suma = num1 + num2;
+ alert(suma)
+ 
+}       
